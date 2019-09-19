26041ae8181633510f5526b8098ab84f4ef429c7
jelkss88888888 <51755796+jelkss88888888@users.noreply.github.com>
2019-09-19 22:43:41 +0900

    1.9 arrays

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/*
+const myname = "Q";
+console.log(myname);
+*/
+const wow = "Fucking awesome";
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", true, false, wow];
+
+console.log(daysOfWeek);
