afe3225c8e7dd51a37e0ab116351315d0e6618be
jelkss88888888 <51755796+jelkss88888888@users.noreply.github.com>
2019-09-23 20:51:56 +0900

    2.1
    array object function

--- scripts.js
+++ scripts.js
@@ -2,7 +2,48 @@
 const myname = "Q";
 console.log(myname);
 */
+
+/* Array, Object
 const wow = "Fucking awesome";
 const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", true, false, wow];
 
 console.log(daysOfWeek);
+
+const octagon = {
+    name: "octagon",
+    location: "gangnam",
+    type: "club",
+    pay: "120,000₩/day",
+    work: ["bartender", "server", "info"],
+    workers: [
+        { bartender: ["uiju", "somebody"] },
+        { server: "sungtaek" },
+        { info: "janghun" }
+    ]
+};
+
+console.log(octagon);
+
+console.log(octagon.type);
+
+console.log(octagon.work);
+
+console.log(octagon.workers);
+
+console.log(octagon.workers[0].bartender[1]);
+*/
+
+/*Function
+
+function helloMacro() {
+    console.log("Hello, Fucking hello my friend?");
+};
+
+helloMacro();
+
+function fruit(apple, banana, grape) {
+    console.log("what fruits do you like? you like", apple, banana, grape, "or something?");
+}
+
+fruit("hi","hello","bye");
+*/
